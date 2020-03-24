791b44cd4a4dfb4dc1032a3c3140fe96754d5a5e
Fidel Thomet <me@fidelthomet.com>
2020-03-24 11:04:13 +0100

    [Update] refined scrollytelling step sizes

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -30,14 +30,12 @@
           <IntersectionObserver class="io-3" :step="3" align="right" :class="{active: step === 3}">
             <p class="tiny">And at <span class="highlight no-hover red">+2°C</span> 14% of the todays population – about 1 billion people – would be exposed.</p>
           </IntersectionObserver>
-          <IntersectionObserver class="io-4" :step="4" align="left">
-            <span/>
-          </IntersectionObserver>
         </section>
       </template>
     </LayoutScrollytelling>
     <section class="wide">
       <p>
+        Above we looked at exposure to at least one of a range of extreme events. There is however a high variety in intensity and severity across these events.
         The extent and impact of crop failures, river floods, tropical cyclones, heatwaves, wildfires, and droughts depend on the underlying meteorological, hydrological, or climatological events as well as on human factors such as land use, water and agricultural management, and population density. Historical variations in these human factors impede the detection of climate change signals in observational records of hazards and exposure.
       </p>
       <p>
@@ -257,19 +255,22 @@ export default {
       &.io-0 {
         margin-top: 50vh;
         padding-top: 20vh;
+        padding-bottom: 10vh;
       }
 
       &.io-1 {
-        padding-top: 20vh;
+        padding-top: 30vh;
+        padding-bottom: 10vh;
       }
 
       &.io-2 {
-        padding-top: 10vh;
+        padding-top: 30vh;
         padding-bottom: 10vh;
       }
 
       &.io-3 {
-        padding-top: 25vh;
+        padding-top: 30vh;
+        padding-bottom: 60vh;
         // margin-bottom: -5vh;
       }
 
